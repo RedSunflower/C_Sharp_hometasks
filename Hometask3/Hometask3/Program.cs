680fe9817f54116ff20a7d0c1221c992d940090a
RedSunFlower <tanyablin@gmail.com>
2017-11-14 18:43:06 +0200

    Updated hometask

--- Hometask3/Hometask3/Program.cs
+++ Hometask3/Hometask3/Program.cs
@@ -22,6 +22,7 @@ namespace Hometask3
                 Console.WriteLine("You entered not a number!!");
                 Console.ForegroundColor = ConsoleColor.White;
             }
+
             Shape[] shapes = new Shape[userArrayLength];
             if (userArrayLength == 0)
             {
@@ -29,59 +30,52 @@ namespace Hometask3
                 Console.WriteLine("Error! Array cannot be of a length 0!");
                 Console.ReadKey();
                 return;
-                    
+
             }
             bool isFull = true;
             int count = 0;
             while (isFull)
             {
-                Console.WriteLine("Enter name of a class");
+                Console.WriteLine("Enter name of a class : Circle or Square or Display to see created elements ");
                 string className = Console.ReadLine();
-                                 
-                            
+
+
                 if (className == "Circle")
                 {
                     Console.WriteLine(" You are creating an array of elements of Circle Class!!");
                     shapes[count] = new Circle(12, "Circle");
                     count++;
-                     
-                    
                 }
                 else if (className == "Square")
                 {
                     Console.WriteLine(" You are creating an array of elements of Square Class!!");
                     shapes[count] = new Square(10, "Square");
                     count++;
-                      
                 }
+
                 if (count == userArrayLength || count > userArrayLength)
                 {
                     isFull = false;
                 }
-                for (int i = 0; i < count; i++)
+                if (className == "Display")
                 {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine("{0} ", shapes[i].Name);
-                    Console.ForegroundColor = ConsoleColor.White;
-
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("{0} ", shapes[i].Name);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
                 }
-
-
             }
-            
+
             for (int i = 0; i < shapes.Length; i++)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("{0} ", shapes[i].Name);
-              
             }
-
-
             Console.ReadKey();
         }
-        
-        }
-    
+    }
 }
 
 
